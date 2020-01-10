27792fa007a65bcadbd4f140065d21ac439d2986
Jeffrey Zhao <jeffreyzhao5618@gmail.com>
2020-01-10 13:32:54 -0600

    Made close button work. Added scrolling on overflow for project popup.

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
+body = document.querySelector('body');
 project1_button = document.querySelector('.project-cell-container .project-cell:nth-child(1)');
-project1_popup = document.getElementById('project1')
+project1_popup = document.getElementById('project1');
+project1_close_butt = document.querySelector('#project1 .project-popup .close-dot');
+console.log(project1_close_butt);
 
-console.log(project1_popup);
 project1_button.addEventListener('click', () => {
     project1_popup.style.display = 'block';
-    console.log('oioo');
+    body.classList.add('stop-scrolling');
 });
+
+project1_close_butt.addEventListener('click', () => {
+    project1_popup.style.display = 'none';
+    body.classList.remove('stop-scrolling');
+})
